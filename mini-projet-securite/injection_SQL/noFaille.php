b433908087aa867d4be93a216aab122f0451f53b
Maxime MEYER <meyer.maxime212@gmail.com>
2021-11-08 19:19:36 +0100

    new commit

--- mini-projet-securite/injection_SQL/noFaille.php
+++ mini-projet-securite/injection_SQL/noFaille.php
@@ -3,8 +3,8 @@ $user = $_POST['Username'];
 $pwd = $_POST['Password'];
 
 $db = new PDO('sqlite:data.db');
-$req = $db->query('SELECT * FROM connexion WHERE id ="'.$user.'" AND mdp = "' .$pwd.'";');
-$req = $req->execute(array($user, $pwd));
+$req = $db->prepare("SELECT * FROM connexion WHERE id = ? AND mdp = ?;");
+$req->execute(array($user, $pwd));
 $ligne = $req->fetchAll(PDO::FETCH_ASSOC);
 
 if(sizeof($ligne) > 0) {
